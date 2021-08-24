8880bfa79477859fd3655c9ca8e9e60f3de6c872
MekkaLabs <87154227+MekkaLabs@users.noreply.github.com>
2021-08-24 19:18:11 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,21 +12,23 @@ export default class MyDocument extends Document {
                         <meta name="facebook-domain-verification" content="shivkloyttu01z8689d99qo334z8dk" />
                         <link rel="icon" href="/favicon.svg" />
 
-                        <script dangerouslySetInnerHTML={{
-                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        <div>
+                            <script dangerouslySetInnerHTML={{
+                                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                             })(window,document,'script','dataLayer','GTM-WK47GTF');`}}></script>
-
+                        </div>
                     </div>
                 </Head>
                 <body>
                     <div>
-                        <noscript dangerouslySetInnerHTML={{
-                            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WK47GTF"
+                        <div>
+                            <noscript dangerouslySetInnerHTML={{
+                                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WK47GTF"
                             height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
-
+                        </div>
                         <Main />
                         <NextScript /></div>
                 </body>
