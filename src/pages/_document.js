334baba6c621ad71dbbdc5f7654e97b2015273b0
MekkaLabs <87154227+MekkaLabs@users.noreply.github.com>
2021-08-24 18:18:19 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,25 +1,28 @@
 
 
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+
 export default class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
-                    <div>
-                        <title>Imecanica</title>
-                        <meta name="description" content="Oficina Móvel" />
-                        <meta name="facebook-domain-verification" content="shivkloyttu01z8689d99qo334z8dk" />
-                        <link rel="icon" href="/favicon.svg" />
+                    <head>
+                        <div>
+                            <title>Imecanica</title>
+                            <meta name="description" content="Oficina Móvel" />
+                            <meta name="facebook-domain-verification" content="shivkloyttu01z8689d99qo334z8dk" />
+                            <link rel="icon" href="/favicon.svg" />
 
-                        <script dangerouslySetInnerHTML={{
-                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            <script dangerouslySetInnerHTML={{
+                                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                             })(window,document,'script','dataLayer','GTM-WK47GTF');`}}></script>
 
-                    </div>
+                        </div>
+                    </head>
                 </Head>
                 <body>
                     <div>
