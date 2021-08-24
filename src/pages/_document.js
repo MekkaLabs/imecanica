aa408722c45eaabb8a709728c496ec5b14b85f4d
MekkaLabs <87154227+MekkaLabs@users.noreply.github.com>
2021-08-24 18:57:59 -0300

    volta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,14 @@
 
 
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
 export default class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
                     <div>
-                        <title>Imecanica | Oficina Móvel</title>
-                        <meta name="description" content="Revisão do seu carro no conforto da sua casa" />
+                        <title>Imecanica</title>
+                        <meta name="description" content="Oficina Móvel" />
                         <meta name="facebook-domain-verification" content="shivkloyttu01z8689d99qo334z8dk" />
                         <link rel="icon" href="/favicon.svg" />
 
@@ -18,9 +17,8 @@ export default class MyDocument extends Document {
                             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-WK47GTF');`}}>
+                            })(window,document,'script','dataLayer','GTM-WK47GTF');`}}></script>
 
-                        </script>
                     </div>
                 </Head>
                 <body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,13 +17,6 @@ const Home: NextPage = () => {
         <meta name="facebook-domain-verification" content="shivkloyttu01z8689d99qo334z8dk" />
         <link rel="icon" href="/favicon.svg" />
 
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-WK47GTF');`}}>
-        </script>
       </Head>
 
       <main>
