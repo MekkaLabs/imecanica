c6150ccbdfd9e9d8d2b9969294fa463e0174596f
MekkaLabs <87154227+MekkaLabs@users.noreply.github.com>
2021-08-24 19:41:54 -0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 
 
-
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }) {
-
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-WK47GTF' });
+  }, []);
   return (
 
     <div>
